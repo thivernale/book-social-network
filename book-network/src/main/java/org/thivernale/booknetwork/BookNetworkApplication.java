d858fc0b885150ec0690078d8711616cfd909c85
thivernale <thivernale@gmail.com>
2024-11-19 17:14:50 +0200

    :tada: Initial Commit - project structure and backend dependencies

---
+++ book-network/src/main/java/org/thivernale/booknetwork/BookNetworkApplication.java
@@ -0,0 +1,12 @@
+package org.thivernale.booknetwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookNetworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookNetworkApplication.class, args);
+    }
+}

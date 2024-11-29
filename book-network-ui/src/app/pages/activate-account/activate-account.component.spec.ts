8517f0e5868abd9f2afc474d1c12c335bbebfc77
thivernale <thivernale@gmail.com>
2024-11-29 10:34:12 +0200

    :test_tube: Add out of place test for practice

--- book-network-ui/src/app/pages/activate-account/activate-account.component.spec.ts
+++ book-network-ui/src/app/pages/activate-account/activate-account.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { provideHttpClient } from '@angular/common/http';
-import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
+import { firstValueFrom } from 'rxjs';
 
 import { ActivateAccountComponent } from './activate-account.component';
 import { AuthenticationService } from '../../services/services';
@@ -8,6 +9,7 @@ import { AuthenticationService } from '../../services/services';
 describe('ActivateAccountComponent', () => {
   let component: ActivateAccountComponent;
   let fixture: ComponentFixture<ActivateAccountComponent>;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -19,9 +21,31 @@ describe('ActivateAccountComponent', () => {
     fixture = TestBed.createComponent(ActivateAccountComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    httpTestingController = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should confirm', () => {
+    const authService = TestBed.inject(AuthenticationService);
+    const service$ = authService.confirm({ token: '123456' });
+
+    const promise = firstValueFrom(service$);
+
+    const req = httpTestingController.expectOne(authService.rootUrl + '' + AuthenticationService.ConfirmPath + '?token=123456', 'Request to activate account');
+
+    expect(req.request.method).toBe('GET');
+    req.flush(null, { status: 200, statusText: 'OK' });
+
+    promise.then(value => {
+      expect(value).toBeNull();
+    });
   });
 });
